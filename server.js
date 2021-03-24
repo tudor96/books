fc2a3feeeedb14587b9005a3a9cc8adb0a91fbae
Golcea Tudor <gltudor96@gmail.com>
2021-03-24 22:39:58 +0200

    removed comments

--- server.js
+++ server.js
@@ -6,23 +6,10 @@ const routes = require("hapi-auto-routes");
 const mongoose = require("mongoose");
 const mongoload = require("mongoload");
 
-// Setting up Server ---------------------------- //
-// const server = new Hapi.Server();
-// server.connection({
-//   port: 3000,
-//   host: "localhost",
-//   routes: {
-//     cors: true,
-//   },
-// });
-
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-// if (process.env.NODE_ENV !== "production") {
-//   mongoose.set("debug", true);
-// }
 
 const db = mongoose.connection;
 
@@ -36,50 +23,14 @@ db.on("error", () => {
 });
 
 db.once("open", function () {
-  // we're connected!
   console.info(`MongoDB::Connected at ${process.env.MONGO_URI}`);
   init();
 });
-
-// db.on("connected", () => {
-//   // On successfull connection, log connection details
-//   console.info(`MongoDB::Connected at ${process.env.MONGO_URI}`);
-//   init();
-//   // start the server
-//   // eslint-disable-next-line
-//   //   startServer();
-// });
 
 db.on("disconnected", (err) => {
   console.error("Connection terminated to db", err);
   process.exit(0);
 });
-
-// HELPER FUNCTIONS ----------------------------- //
-// function startServer() {
-
-//   server.register(requiredHapiPlugins, (err) => {
-//     if (err) {
-//       console.error('Error in registering one or more plugins.');
-
-//       process.exit(0);
-//     } else {
-//       server.auth.strategy('jwt', 'jwt', {
-//         key: config.JWTConfig.secret,
-//         validateFunc: jwtValidate,
-//         verifyOptions: { algorithms: ['HS256'] },
-//       });
-
-//       routes.bind(server).register({
-//         pattern: `${__dirname}/routes/**/*.js`,
-//       });
-
-//       server.start(() => {
-//         console.info(`Server started at: ${server.info.uri}`);
-//       });
-//     }
-//   });
-// }
 
 const init = async () => {
   const server = Hapi.server({
@@ -90,14 +41,6 @@ const init = async () => {
   routes.bind(server).register({
     pattern: `${__dirname}/routes/**/*.js`,
   });
-
-  //   server.route({
-  //     method: "GET",
-  //     path: "/",
-  //     handler: (request, h) => {
-  //       return "Hello World!";
-  //     },
-  //   });
 
   await server.start();
   console.log("Server running on %s", server.info.uri);
